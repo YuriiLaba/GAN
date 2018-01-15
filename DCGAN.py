fbcf29b722e6629609b9c5bbb992c369a74d766c
DzvinkaYarish <yarish@ucu.edu.ua>
2018-01-15 15:33:50 +0200

    added DCGAN archtecture for colorizing

--- DCGAN.py
+++ DCGAN.py
@@ -12,7 +12,7 @@ from manipulation import save_images
 
 from dataloader import *
 
-DIM = 32
+DIM = 128
 
 def load_data(lower_bound=0, upper_bound=1000):
     batch_size = 100
@@ -31,40 +31,61 @@ def load_data(lower_bound=0, upper_bound=1000):
 class Discriminator(nn.Module):
     def __init__(self, dim, channels=1):
         super().__init__()
-        self.conv_1 = nn.Conv2d(channels, dim // 4, 5, stride=2, padding=2)
-        self.conv_2 = nn.Conv2d(dim // 4, dim // 2, 5, stride=2, padding=2)
-        self.conv_3 = nn.Conv2d(dim // 2, dim, 5, stride=2, padding=2)
-        self.conv_2_bn = nn.BatchNorm2d(dim // 2)
-        self.conv_3_bn = nn.BatchNorm2d(dim)
+        self.conv_1 = nn.Conv2d(1, 32, 3, stride=2, padding=2)
+        self.conv_2 = nn.Conv2d(32, 64, 3, stride=2, padding=2)
+        self.conv_3 = nn.Conv2d(64, 128, 3, stride=2, padding=2)
+        self.conv_4 = nn.Conv2d(128, 128, 3, stride=2, padding=2)
+        self.conv_1_bn = nn.BatchNorm2d(32)
+        self.conv_2_bn = nn.BatchNorm2d(64)
+        self.conv_3_bn = nn.BatchNorm2d(128)
+
         # should be corrected if new image arrive
-        self.fc = nn.Linear(dim * 4 * 4, 1)
+        # self.fc = nn.Linear(dim * 4 * 4, 1)
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv_1(x), 0.2)
-        x = F.leaky_relu(self.conv_2_bn(self.conv_2(x)), 0.2)
-        x = F.leaky_relu(self.conv_3_bn(self.conv_3(x)), 0.2)
+        x = F.leaky_relu(self.conv_1(x), 0.1)
+        x = F.leaky_relu(self.conv_2_bn(self.conv_2(x)), 0.1)
+        x = F.leaky_relu(self.conv_3_bn(self.conv_3(x)), 0.1)
         # should be corrected if new image arrive
         x = x.view(-1, self.dim * 4 * 4)
-        x = F.sigmoid(self.fc(x))
+        # x = F.sigmoid(self.fc(x))
+        x = F.sigmoid((self.conv_4(x)))
         return x
 
 class Generator(nn.Module):
     def __init__(self, dim, channels=1):
         super().__init__()
         self.dim = dim
-        self.fc = nn.Linear(14 * 14, 4 * 4 * dim)
-        self.conv_1 = nn.ConvTranspose2d(dim, dim // 2, 5, stride=2, padding=2)
-        self.conv_2 = nn.ConvTranspose2d(dim // 2, dim // 4, 6, stride=2, padding=2)
-        self.conv_3 = nn.ConvTranspose2d(dim // 4, channels, 6, stride=2, padding=2)
-        self.conv_1_bn = nn.BatchNorm2d(dim // 2)
-        self.conv_2_bn = nn.BatchNorm2d(dim // 4)
+        # self.fc = nn.Linear(14 * 14, 4 * 4 * dim)
+        self.conv_1 = nn.Conv2d(1, 32, 3, stride=2, padding=2)
+        self.conv_2 = nn.Conv2d(32, 64, 3, stride=2, padding=2)
+        self.conv_3 = nn.Conv2d(64, 128, 3, stride=2, padding=2)
+        self.conv_4 = nn.Conv2d(128, 128, 3, stride=2, padding=2)
+
+        self.deconv_1 = nn.ConvTranspose2d(128, 128, 3, stride=2, padding=2)
+        self.deconv_2 = nn.ConvTranspose2d(128, 64, 3, stride=2, padding=2)
+        self.deconv_3 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=2)
+        self.deconv_4 = nn.ConvTranspose2d(32, 3, 3, stride=2, padding=2)
+
+        self.conv_1_bn = nn.BatchNorm2d(32)
+        self.conv_2_bn = nn.BatchNorm2d(64)
+        self.conv_3_bn = nn.BatchNorm2d(128)
+
 
     def forward(self, x):
-        x = self.fc(x)
+        # x = self.fc(x)
         x = x.view(-1, self.dim, 4, 4)
-        x = F.relu(self.conv_1_bn(self.conv_1(x)))
-        x = F.relu(self.conv_2_bn(self.conv_2(x)))
-        x = F.tanh(self.conv_3(x))
+        x = F.leaky_relu(self.conv_1_bn(self.conv_1(x)))
+        x = F.leaky_relu(self.conv_2_bn(self.conv_2(x)))
+        x = F.leaky_relu(self.conv_3_bn(self.conv_3(x)))
+        x = F.leaky_relu(self.conv_3_bn(self.conv_4(x)))
+
+        x = F.leaky_relu(self.conv_1_bn(self.deconv_1(x)))
+        x = F.leaky_relu(self.conv_2_bn(self.deconv_2(x)))
+        x = F.leaky_relu(self.conv_3_bn(self.deconv_3(x)))
+
+
+        x = F.tanh(self.deconv_4(x))
 
         return x
 
