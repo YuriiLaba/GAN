df2cc777973e6f257ac46ed9be591343ee744c6a
DzvinkaYarish <yarish@ucu.edu.ua>
2018-01-28 19:18:09 +0200

    updated GAN architecture

--- DCGAN_with_skip_connection.py
+++ DCGAN_with_skip_connection.py
@@ -1,26 +1,11 @@
-#COMMON GAN WITH FULY CONNECTED LAYERS USED FOR IMAGE GENERATION
-
-from PIL import Image
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.optim import Adam
 from torchvision import transforms
-import torchvision.datasets as dsets
 import torch.nn.functional as F
-import numpy as np
-from vgg import Vgg16
 from manipulation import save_images
-import torchvision.models as models
-
-from dataloader import *
-
-def gram_matrix(y):
-    (b, ch, h, w) = y.size()
-    features = y.view(b, ch, w * h)
-    features_t = features.transpose(1, 2)
-    gram = features.bmm(features_t) / (ch * h * w)
-    return gram
+from dataloader import ImageDataset
 
 def load_data(path, lower_bound=0, upper_bound=1000):
     batch_size = 100
@@ -29,7 +14,7 @@ def load_data(path, lower_bound=0, upper_bound=1000):
         transforms.ToTensor(),
         # transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
-    dataset = ImageDataset(path + "Cats_color_128", path + "Cats_B&W_128", transform=transform, lower_bound=lower_bound, upper_bound=upper_bound)
+    dataset = ImageDataset(path + "Cats_color_128", transform=transform, lower_bound=lower_bound, upper_bound=upper_bound)
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return loader
 
@@ -38,29 +23,25 @@ def load_data(path, lower_bound=0, upper_bound=1000):
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv_1 = nn.Conv2d(3, 32, 3, stride=2, padding=2)
-        self.conv_2 = nn.Conv2d(32, 64, 3, stride=2, padding=2)
-        self.conv_3 = nn.Conv2d(64, 128, 3, stride=2, padding=2)
-        self.conv_4 = nn.Conv2d(128, 64, 3, stride=2, padding=2)
-        self.conv_5 = nn.Conv2d(64, 1, 3, stride=2, padding=2)
+        self.conv_1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
+        self.conv_2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
+        self.conv_3 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
+        self.conv_4 = nn.Conv2d(128, 64, 3, stride=2, padding=0)
+        self.conv_5 = nn.Conv2d(64, 64, 5, stride=2, padding=0)
+        self.conv_6 = nn.Conv2d(64, 1, 1, stride=2, padding=0)
         self.conv_1_bn = nn.BatchNorm2d(32)
         self.conv_2_bn = nn.BatchNorm2d(64)
         self.conv_3_bn = nn.BatchNorm2d(128)
         self.conv_4_bn = nn.BatchNorm2d(64)
-        self.fc = nn.Linear(1000, 1)
-
-        # should be corrected if new image arrive
-        # self.fc = nn.Linear(dim * 4 * 4, 1)
+        self.conv_5_bn = nn.BatchNorm2d(64)
 
     def forward(self, x):
         x = F.leaky_relu(self.conv_1(x), 0.1)
         x = F.leaky_relu(self.conv_2_bn(self.conv_2(x)), 0.1)
         x = F.leaky_relu(self.conv_3_bn(self.conv_3(x)), 0.1)
         x = F.leaky_relu(self.conv_4_bn(self.conv_4(x)), 0.1)
-        x = self.conv_5(x)
-        x = x.view(x.size(0), -1).mean(1)
-        x = F.sigmoid(x)
-
+        x = F.leaky_relu(self.conv_5_bn(self.conv_5(x)), 0.1)
+        x = F.sigmoid(self.conv_6(x))
         return x
 
 class Generator(nn.Module):
@@ -85,23 +66,14 @@ class Generator(nn.Module):
         self.deconv_3_bn = nn.BatchNorm2d(32)
 
     def forward(self, x):
-        # print('x', x.size())
         x1 = F.relu(self.conv_1(x))
-        # print('x1', x1.size())
         x2 = F.relu(self.conv_2_bn(self.conv_2(x1)))
-        # print('x2', x2.size())
         x3 = F.relu(self.conv_3_bn(self.conv_3(x2)))
-        # print('x3', x3.size())
         x4 = F.relu(self.conv_4_bn(self.conv_4(x3)))
-        # print('x4', x4.size())
         x5 = F.relu(self.deconv_1_bn(self.deconv_1(x4))) + x3
-        # print('x5', x5.size())
         x6 = F.relu(self.deconv_2_bn(self.deconv_2(x5))) + x2
-        # print('x6', x6.size())
         x7 = F.relu(self.deconv_3_bn(self.deconv_3(x6))) + x1
-        # print('x7', x7.size())
         x8 = F.tanh(self.deconv_4(x7))
-        # print('x8', x8.size())
         return x8
 
 
@@ -130,8 +102,9 @@ def train_GAN(use_cuda=False):
     g_optimizer = Adam(generator.parameters(), lr=lr, betas=betas)
     criterion_BCE = nn.BCELoss()
     criterion_L1 = nn.L1Loss()
+    criterion_MSE = nn.MSELoss()
 
-    num_epochs = 100
+    num_epochs = 20
     num_of_samples = 100
 
     for epoch in range(num_epochs):
@@ -154,9 +127,9 @@ def train_GAN(use_cuda=False):
             out = discriminator(generated_images)
 
 
-            loss_img = criterion_L1(generated_images, color_images)
+            loss_img = criterion_MSE(generated_images, color_images)
             loss_1 = criterion_BCE(out, labels_1)
-            g_loss = loss_1 + loss_img
+            g_loss = loss_1 + 100 * loss_img
             # g_loss = styleloss + loss_1
             g_loss.backward()
             g_optimizer.step()
@@ -183,9 +156,9 @@ def train_GAN(use_cuda=False):
         filename_bw = "/output/epoch_{}/black_and_white/" if use_cuda else "samples/epoch_{}/black_and_white/"
         filename_color = "/output/epoch_{}/incolor/" if use_cuda else "samples/epoch_{}/incolor/"
 
-        save_images(test_images_colored.data.cpu().numpy(), path=filename_colored.format(epoch + 1))
-        save_images(test_images_bw.data.cpu().numpy(), path=filename_bw.format(epoch + 1))
-        save_images(test_images_color.numpy(), path=filename_color.format(epoch + 1))
+        save_images(test_images_colored.data.cpu().numpy(), filename=filename_colored.format(epoch + 1))
+        save_images(test_images_bw.data.cpu().numpy(), filename=filename_bw.format(epoch + 1))
+        save_images(test_images_color.numpy(), filename=filename_color.format(epoch + 1))
 
 
 if __name__ == "__main__":
--- dataloader.py
+++ dataloader.py
@@ -1,29 +1,23 @@
 import torch
 import torch.utils.data as data
 import os
-import numpy as np
 
 from torchvision import transforms
 from PIL import Image
 
 class ImageDataset(data.Dataset):
-    def __init__(self, color_images_path, b_and_w_images_path, lower_bound=0, upper_bound=1000, transform=None):
+    def __init__(self, color_images_path, lower_bound=0, upper_bound=1000, transform=None):
         self.color_images = color_images_path
-        self.b_and_w_images = b_and_w_images_path
         self.transform = transform
         self.images_color_lst = []
-        self.images_b_and_w_lst = []
+
 
         filenames_colour = os.listdir(color_images_path)
-        # filenames_bw = os.listdir(b_and_w_images_path)
 
         upper_bound = min(len(filenames_colour), upper_bound)
 
-        for file_name in filenames_colour[lower_bound: upper_bound]:
+        for file_name in filenames_colour[lower_bound:upper_bound]:
             self.images_color_lst.append(color_images_path + "/" + file_name)
-
-        # for file_name in filenames_colour[lower_bound: upper_bound]:
-        #     self.images_b_and_w_lst.append(b_and_w_images_path + "/" + file_name)
 
 
     def __len__(self):
@@ -46,10 +40,7 @@ if __name__ == "__main__":
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
-    dataset = ImageDataset("/home/yuriy/Cats/Cats_color", "/home/yuriy/Cats/Cats_B&W")
+    dataset = ImageDataset("/home/yuriy/Cats/Cats_color")
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    # print(len(loader.dataset))
-    # print(dataset.images_b_and_w)
     print(len(dataset.images_color_lst))
-    print(len(dataset.images_b_and_w_lst))
-    # print(dataset.images[letter[1]])
+
